cda1539fcef3999ad29cdef8fe935de3adef3921
ahmedjp143 <ajb1434@gmail.com>
2023-08-21 08:16:44 +0100

    commit secod

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import Dashboard from './dashboard/Dashboard';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      
-         <h1>hi ahmed</h1>
+      <Dashboard />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/dashboard/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Dashboard() {
+  return <div>Dashboa</div>;
+}
+
+export default Dashboard;
